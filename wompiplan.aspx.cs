2f1e0d943b5e39141d76bcfa72c38837e93a82a9
FullStackFitnessPeople <fullstack@fitnesspeoplecmd.com>
2025-08-29 10:06:17 -0500

    CR Cambio de URL en wompiplan

--- wompiplan.aspx.cs
+++ wompiplan.aspx.cs
@@ -41,7 +41,7 @@ namespace WebPage
 
             string strString = Convert.ToBase64String(Encoding.Unicode.GetBytes(strDocumento));
 
-            _strRedireccion = "https://localhost:44382/wompidata?code=" + strString;
+            _strRedireccion = "https://fitnesspeoplecolombia.com/wompidata?code=" + strString;
         }
 
         static string ComputeSha256Hash(string rawData)
