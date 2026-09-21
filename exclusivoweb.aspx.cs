6680ced73c839dbff6331dc90338471040686de7
Fullstack <fullstack@fitnesspeoplecmd.com>
2026-09-21 08:04:05 -0500

    CR Se ocultó el plan Flexible Pro Promo en landing ExclusivoWeb

--- exclusivoweb.aspx.cs
+++ exclusivoweb.aspx.cs
@@ -72,32 +72,32 @@ namespace WebPage
                 // ===============================
                 // PLAN FLEXIBLE PRO PROMO [45]
                 // ===============================
-                case "flexible-pro-promo":
+                //case "flexible-pro-promo":
 
-                    lblTitulo.InnerText = "PLAN FLEXIBLE PRO";
-                    lblSubTitulo.InnerText = "PLAN DÉBITO AUTOMÁTICO";
+                //    lblTitulo.InnerText = "PLAN FLEXIBLE PRO";
+                //    lblSubTitulo.InnerText = "PLAN DÉBITO AUTOMÁTICO";
 
-                    lblTituloPrecio.InnerHtml = "PRIMER MES <br /> $ 9.900";
-                    lblSubTituloPrecio1.InnerText = "SIN INSCRIPCIÓN";
-                    lblSubTituloPrecio2.InnerText = "DESPUÉS $99.000/MES";
-                    lblSubTituloUp.InnerText = "ANTES $165.000";
-                    lblFidelidad.InnerText = "FIDELIDAD MÍNIMA DE 12 MESES";
-                    lblNotaFidelidad.InnerText = "APLICA COBRO POR RETIRO ANTICIPADO";
+                //    lblTituloPrecio.InnerHtml = "PRIMER MES <br /> $ 9.900";
+                //    lblSubTituloPrecio1.InnerText = "SIN INSCRIPCIÓN";
+                //    lblSubTituloPrecio2.InnerText = "DESPUÉS $99.000/MES";
+                //    lblSubTituloUp.InnerText = "ANTES $165.000";
+                //    lblFidelidad.InnerText = "FIDELIDAD MÍNIMA DE 12 MESES";
+                //    lblNotaFidelidad.InnerText = "APLICA COBRO POR RETIRO ANTICIPADO";
 
-                    divPackBienvenida.Visible = false;
+                //    divPackBienvenida.Visible = false;
 
-                    itemId = "2";
-                    itemName = "Plan Flexible Pro Promo";
-                    price = 9900;
-                    texto = "ACTIVA TU PLAN";
-                    tokenId = "register?token=TKIlFPP8XYRC9l1rfGjR";
-                    urlKey = "";
-                    ConfigurarBtn(lnkComprar1, texto, itemId, itemName, price, tokenId, urlKey);
-                    ConfigurarBtn(lnkComprar3, texto, itemId, itemName, price, tokenId, urlKey);
+                //    itemId = "2";
+                //    itemName = "Plan Flexible Pro Promo";
+                //    price = 9900;
+                //    texto = "ACTIVA TU PLAN";
+                //    tokenId = "register?token=TKIlFPP8XYRC9l1rfGjR";
+                //    urlKey = "";
+                //    ConfigurarBtn(lnkComprar1, texto, itemId, itemName, price, tokenId, urlKey);
+                //    ConfigurarBtn(lnkComprar3, texto, itemId, itemName, price, tokenId, urlKey);
 
-                    lblTextoFinal.InnerHtml = "Pago mensual mediante débito automático. <br /> No aplica para pagos en efectivo, transferencia ni datáfono.";
+                //    lblTextoFinal.InnerHtml = "Pago mensual mediante débito automático. <br /> No aplica para pagos en efectivo, transferencia ni datáfono.";
 
-                    break;
+                //    break;
 
                 // ===============================
                 // PLAN SORPRÉNDETE EN DICIEMBRE
